125488f4c050506f8e5ed5578545b9cad1a7f69f
Dan Zhang <dzhang50@hotmail.com>
2015-03-15 02:41:58 -0500

    Done calculation support, removed some debug output, writes output to file

--- MX/SSSP/C_src/App_SW.cpp
+++ MX/SSSP/C_src/App_SW.cpp
@@ -43,7 +43,7 @@ typedef enum {
               PARAM_JOBSPTR,
               PARAM_META,
               PARAM_OUTPUT,
-              PARAM_STATUS,
+              PARAM_DONE,
               PARAM_SENTINEL
 } ParamIndexes;
 
@@ -102,6 +102,13 @@ struct Output {
     uint32_t result;
 };
 
+struct Done {
+    uint32_t done0;
+    uint32_t done1;
+    uint32_t done2;
+    uint32_t done3;
+};
+
 // ASSUMES SRC EDGES ARE IN ORDER
 void readGraph(const char* file, Node **nodes, uint32_t &numNodes, Edge **edges, uint32_t &numEdges) {
     
@@ -119,7 +126,7 @@ void readGraph(const char* file, Node **nodes, uint32_t &numNodes, Edge **edges,
     uint32_t edgeIdx = 0;
     while(in.good()) {
         in >> src >> dest >> weight;
-        std::cout << "---" << src << " " << dest << " " << weight << "\n";
+        //std::cout << "---" << src << " " << dest << " " << weight << ", idx: " << edgeIdx << "\n";
         
         // If new node
         if(src != lastNode) {
@@ -132,6 +139,8 @@ void readGraph(const char* file, Node **nodes, uint32_t &numNodes, Edge **edges,
         
         // Append new edge
         assert(edgeIdx < numEdges);
+        if(edgeIdx >= numEdges)
+            printf("ERROR: node %d edgeIdx %d is not less than numEdges %d (src %d, dest %d, weight %d)\n", src, edgeIdx, numEdges, src, dest, weight); 
         (*edges)[edgeIdx].dest = dest;
         (*edges)[edgeIdx].weight = weight;
         (*nodes)[src].numEdges += 1;
@@ -155,6 +164,8 @@ int App_SW (const char *file)
     int        j, fpga, n_errs;
     
     
+    std::ofstream out("output");
+    
     // Generate and create Graph data structure
     Node *nodes, *cp_nodes;
     Edge *edges, *cp_edges;
@@ -163,12 +174,12 @@ int App_SW (const char *file)
     readGraph(file, &nodes, numNodes, &edges, numEdges);
     printf("Done reading graph\n");
     printf("numNodes: %d, numEdges: %d\n", numNodes, numEdges);
-    for(int i = 0; i < numNodes; i++)
+    /*for(int i = 0; i < numNodes; i++)
         printf("  nodes[%d] = {%d, %d, %d, %d}\n", i, nodes[i].id, nodes[i].payload, nodes[i].edgePtr, nodes[i].numEdges);
     
     for(int i = 0; i < numEdges; i++)
         printf("  edges[%d] = {%d, %d}]\n", i, edges[i].dest, edges[i].weight);
-        
+    */
     // Generate and create Worklist data structure
     
     Job *jobs, *cp_jobs;
@@ -196,17 +207,23 @@ int App_SW (const char *file)
     
     output = (Output*)calloc(1, sizeof(Output));
     
+    // Done signals
+    Done *done, *cp_done;
+    
+    done = (Done*)calloc(1, sizeof(Done));
+    
     // ----------------------------------------------------------------
     // Allocate the input and output vectors on HW-side memory
     printf("Allocating CNY memory\n");
-    for(int i = 0; i < numNodes*sizeof(Node)/4; i++)
-        printf("%d\n", ((int*)nodes)[i]);
+    //for(int i = 0; i < numNodes*sizeof(Node)/4; i++)
+    //    printf("%d\n", ((int*)nodes)[i]);
     // Malloc on the coproc   
     cny_cp_posix_memalign((void**)&cp_nodes, 512, numNodes*sizeof(Node));
     cny_cp_posix_memalign((void**)&cp_edges, 512, numEdges*sizeof(Edge));
     cny_cp_posix_memalign((void**)&cp_jobs, 512, maxJobs*sizeof(Job));
     cny_cp_posix_memalign((void**)&cp_meta, 512, numMeta*sizeof(uint64_t));
     cny_cp_posix_memalign((void**)&cp_output, 512, 1*sizeof(Output));
+    cny_cp_posix_memalign((void**)&cp_done, 512, 1*sizeof(Done));
     
     // copy the input arrays to coprocessor using datamover
     cny_cp_memcpy (cp_nodes, nodes, numNodes*sizeof(Node));
@@ -214,6 +231,7 @@ int App_SW (const char *file)
     cny_cp_memcpy (cp_jobs, jobs, numJobs*sizeof(Job));
     cny_cp_memcpy (cp_meta, meta, numMeta*sizeof(uint64_t));
     cny_cp_memcpy (cp_output, output, 1*sizeof(Output));
+    cny_cp_memcpy (cp_done, done, 1*sizeof(Done));
     
     // ----------------------------------------------------------------
     // Create the param block, and initialize it
@@ -231,6 +249,7 @@ int App_SW (const char *file)
         param_block [PARAM_JOBSPTR  * 8 + fpga] = ptr_to_ui64(cp_jobs);
         param_block [PARAM_META     * 8 + fpga] = ptr_to_ui64(cp_meta);
         param_block [PARAM_OUTPUT   * 8 + fpga] = ptr_to_ui64(cp_output);
+        param_block [PARAM_DONE     * 8 + fpga] = ptr_to_ui64(cp_done);
         param_block [PARAM_SENTINEL * 8 + fpga] = 0xCAFEF000+fpga;
         printf("%0d\n", PARAM_SENTINEL * 8 + fpga);
         printf ("C: params [fpga %0d] are %0llx 0x%llx 0x%llx %lld 0x%llx 0x%llx\n", fpga,
@@ -239,6 +258,7 @@ int App_SW (const char *file)
                 param_block [PARAM_JOBSPTR  * 8 + fpga],
                 param_block [PARAM_META     * 8 + fpga],
                 param_block [PARAM_OUTPUT   * 8 + fpga],
+                param_block [PARAM_DONE     * 8 + fpga],
                 param_block [PARAM_SENTINEL * 8 + fpga]);
     }
 
@@ -258,13 +278,20 @@ int App_SW (const char *file)
     record_finish_time ();
     printf ("C: returned from HW\n");
     fprint_delta_time (stdout);
-
+    
+    // Check all done
+    cny_cp_memcpy(done, cp_done, 1*sizeof(Done));
+    //assert((dones->done0 == 2) && (dones->done1 == 2) && (dones->done2 == 2) && (dones->done3 == 2));
+    printf("Dones: %d %d %d %d", done->done0, done->done1, done->done2, done->done3);
+    
     // ----------------------------------------------------------------
     // Print out new node payloads
     cny_cp_memcpy (nodes, cp_nodes, numNodes*sizeof(Node));
     for(int i = 0; i < numNodes; i++) {
         printf("  nodes[%d] = {%d, *%d, %d, %d}\n", i, nodes[i].id, nodes[i].payload, nodes[i].edgePtr, nodes[i].numEdges);
+        out << nodes[i].id << "," << nodes[i].payload << "\n";
     }
+    out.close();
     // ----------------------------------------------------------------
     // Stop status monitoring
     // bc_stop_instrumentation ();
