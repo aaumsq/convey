c31959f0d5c42e2ded04ff510978f67f06dd2b7e
Dan Zhang <dan.zhang@utexas.edu>
2015-11-03 22:50:18 -0600

    Now Makeing with -O3. Added proper working randStream benchmark. Added utility functions for PCM

---
+++ Sim/pcm.h
@@ -0,0 +1,86 @@
+#ifndef __PCM_H__
+#define __PCM_H__
+
+
+#include <cpucounters.h>
+
+struct PCMEvent {
+    const char * name;
+    unsigned char event;
+    unsigned char umask;
+    const char * description;
+};
+
+PCM* m;
+
+void initPCM(PCMEvent* WSMEvents) {
+    m = PCM::getInstance();
+    
+    PCM::ExtendedCustomCoreEventDescription conf;
+    conf.fixedCfg = NULL; // default
+    conf.nGPCounters = 4;
+    EventSelectRegister regs[4];
+    conf.gpCounterCfg = regs;
+    EventSelectRegister def_event_select_reg;
+    def_event_select_reg.value = 0;
+    def_event_select_reg.fields.usr = 1;
+    def_event_select_reg.fields.os = 1;
+    def_event_select_reg.fields.enable = 1;
+    for(int i=0;i<4;++i)
+        regs[i] = def_event_select_reg;
+    
+    for(int i = 0; i < 4; i++) {
+        regs[i].fields.event_select = WSMEvents[i].event;
+        regs[i].fields.umask = WSMEvents[i].umask;
+    }
+    
+    PCM::ErrorCode status = m->program(PCM::EXT_CUSTOM_CORE_EVENTS, &conf);
+    switch(status) {
+    case PCM::Success:
+        break;
+    case PCM::MSRAccessDenied:
+        std::cerr << "Access to Intel(r) Performance Counter Monitor has denied (no MSR or PCI CFG space access).\n";
+        exit(EXIT_FAILURE);
+    case PCM::PMUBusy:
+        std::cerr << "Access to Intel(r) Performance Counter Monitor has denied (Performance Monitoring Unit is occupied by other application). Try to stop the application that uses PMU.\n";
+        std::cerr << "Alternatively you can try to reset PMU configuration at your own risk. Try to reset? (y/n)\n";
+        char yn;
+        std::cin >> yn;
+        if ('y' == yn) {
+            m->resetPMU();
+            std::cerr << "PMU configuration has been reset. Try to rerun the program again.\n";
+        }
+        exit(EXIT_FAILURE);
+    default:
+        std::cerr << "Access to Intel(r) Performance Counter Monitor has denied (Unknown error).\n";
+        exit(EXIT_FAILURE);
+    }
+    
+    std::cerr << "\nDetected "<< m->getCPUBrandString() << " \"Intel(r) microarchitecture codename "<<m->getUArchCodename()<<"\"\n";
+    
+}
+
+void printStats(PCMEvent* WSMEvents, SystemCounterState before_sstate, SystemCounterState after_sstate) {
+    uint64_t insts = getInstructionsRetired(before_sstate, after_sstate);
+    uint64_t cycles = getCycles(before_sstate, after_sstate);
+    
+    for(int i = 0; i < 4; i++) {
+        uint64_t counter = getNumberOfCustomEvents(i, before_sstate, after_sstate);
+        std::cout << WSMEvents[i].name << ": " << counter 
+                  << ", PKI: " << double(counter)*1000.0/double(insts) 
+                  << ", PKC: " << double(counter)*1000.0/double(cycles) << "\n";
+    }
+
+    
+    std::cout << "Instructions retired: " << insts << "\n";
+    std::cout << "Cycles: " << cycles << "\n";
+    std::cout << "IPC: " << getIPC(before_sstate, after_sstate) << "\n";
+    std::cout << "Calc IPC: " << double(insts)/double(cycles) << "\n";
+    
+    std::cout << "Instructions per clock: " << getIPC(before_sstate,after_sstate)
+              << "\nL3 cache hit ratio: " << getL3CacheHitRatio(before_sstate,after_sstate)
+              << "\nBytes read: " << getBytesReadFromMC(before_sstate,after_sstate)
+              << "\n";
+}
+
+#endif
---
+++ Sim/randStream.cpp
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include <time.h>
+#include <stdint.h>
+#include <algorithm>
+#include <vector>
+
+//#include "zsim_hooks.h"
+#include "pcm.h"
+
+PCMEvent WSMEvents[4] = {
+    {"RESOURCE_STALLS.ANY", 0xA2, 0x01, "Allocator resource related stalls, including PRF, LSQ, branch mispred, etc."},
+    {"RESOURCE_STALLS.LOAD", 0xA2, 0x02, "Stall cycles due to lack of load buffer entries"},
+    {"RESOURCE_STALLS.RS_FULL", 0xA2, 0x04, "Stall cycles due to lack of reservation station entries"},
+    //{"L1D.PEND_MISS.PENDING", 0x48, 0x01, "Increments the number of outstanding L1D misses every cycle. Set Cmask = 1 and Edge = 1 to count occurrences"}
+    {"RESOURCE_STALLS.ROB_FULL", 0xA2, 0x10, "Stall cycles due to lack of ROB entries"}
+    /*
+    {"LOAD_BLOCK.L1D", 0x03, 0x20, "Loads blocked by the L1 data cache, e.g. too many outstanding misses"},
+    {"DTLB_MISSES.ANY", 0x08, 0x01, "Memory accesses that missed the DTLB"},
+    {"LOAD_DISPATCH.ANY", 0x13, 0x04, "Loads dispatched from the reservation station"},
+    {"BR_MISP_EXEC.ANY", 0x89, 0x7F, "Number of mispredicted near branch insts that were executed, but not necessarily retired"}
+    */
+};
+
+
+int main(int argc, char** argv) {
+    
+    if(argc != 3) {
+      std::cout << "./a.out <total loops> <num streams>\n";
+      exit(1);
+    }
+    unsigned loops = atoi(argv[1]);
+    unsigned streams = atoi(argv[2]);
+    uint64_t nodeIdx[streams];
+    uint64_t sums[streams];
+    
+    uint32_t arraySize = 4*1024*1024; // 64M-entries
+    std::cout << "Loops: " << loops << ", Streams: " << streams << "\n";
+    std::cout << "Trying to initialize array size = " << arraySize/1024 << "K entries = " << arraySize*sizeof(uint32_t)/1024 << "KB\n";
+
+    initPCM(WSMEvents);
+    
+    std::vector<uint32_t> arrayTmp;
+    std::vector<uint32_t> array;
+    std::vector<uint32_t> counts;
+    for(int i = 1; i < arraySize; i++) {
+      arrayTmp.push_back(i); //rand() % arraySize;
+      counts.push_back(0);
+    }
+    counts.push_back(0); // size = arraySize
+    std::random_shuffle(arrayTmp.begin(), arrayTmp.end());
+
+    array.resize(arraySize);
+    uint32_t curIdx = 0;
+    while(arrayTmp.size() > 0) {
+      uint32_t newIdx = arrayTmp[arrayTmp.size()-1];
+      array[curIdx] = newIdx;
+      curIdx = newIdx;
+
+      arrayTmp.pop_back();
+    }
+    array[curIdx] = 0;
+    
+    uint64_t arraySums[streams];
+    uint64_t lastArray[streams];
+    for(int i = 0; i < streams; i++) {
+        arraySums[i] = 0;
+        lastArray[i] = i; //rand() % arraySize;
+    }
+    std::cout << "Starting...\n";
+    //zsim_roi_begin();
+    SystemCounterState before_sstate = getSystemCounterState();
+    for(uint64_t i = 0; i < (loops/streams)*arraySize; i++) {
+        for(int j = 0; j < streams; j++) {
+	    lastArray[j] = array[lastArray[j]];
+	    arraySums[j] += lastArray[j];
+        }
+    }
+    //zsim_roi_end();
+    SystemCounterState after_sstate = getSystemCounterState();
+    printStats(WSMEvents, before_sstate, after_sstate);
+    
+    std::cout << "Results: ";
+    for(int i = 0; i < streams; i++) {
+        std::cout << arraySums[i] << " ";
+    }
+    std::cout << "\n";
+
+    /*
+    std::cout << "Counts: ";
+    for(int i = 0; i < arraySize; i++) {
+      std::cout << counts[i] << " ";
+    }
+    */
+}
