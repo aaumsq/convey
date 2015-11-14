8636d6be4b12255a1512bf4519455162177520f1
Dan Zhang <dan.zhang@utexas.edu>
2015-11-14 17:00:04 -0600

    Updated tests. Added Intel PCM support for Dijkstra

--- Sim/randStream.cpp
+++ Sim/randStream.cpp
@@ -8,44 +8,57 @@
 #include <algorithm>
 #include <vector>
 
-//#include "zsim_hooks.h"
+#define PERFMON
+
+#ifdef ZSIM
+#include "zsim_hooks.h"
+#endif
+
+#ifdef PERFMON
 #include "pcm.h"
 
 PCMEvent WSMEvents[4] = {
-    {"RESOURCE_STALLS.ANY", 0xA2, 0x01, "Allocator resource related stalls, including PRF, LSQ, branch mispred, etc."},
-    {"RESOURCE_STALLS.LOAD", 0xA2, 0x02, "Stall cycles due to lack of load buffer entries"},
-    {"RESOURCE_STALLS.RS_FULL", 0xA2, 0x04, "Stall cycles due to lack of reservation station entries"},
+    //{"RESOURCE_STALLS.ANY", 0xA2, 0x01, "Allocator resource related stalls, including PRF, LSQ, branch mispred, etc."},
+    //{"RESOURCE_STALLS.LOAD", 0xA2, 0x02, "Stall cycles due to lack of load buffer entries"},
+    //{"RESOURCE_STALLS.RS_FULL", 0xA2, 0x04, "Stall cycles due to lack of reservation station entries"},
     //{"RESOURCE_STALLS.STORE", 0xA2, 0x08, "Stall cycles due to lack of store buffer entries"},
     //{"L1D.PEND_MISS.PENDING", 0x48, 0x01, "Increments the number of outstanding L1D misses every cycle. Set Cmask = 1 and Edge = 1 to count occurrences"}
-    {"RESOURCE_STALLS.ROB_FULL", 0xA2, 0x10, "Stall cycles due to lack of ROB entries"},
+    //{"RESOURCE_STALLS.ROB_FULL", 0xA2, 0x10, "Stall cycles due to lack of ROB entries"},
     //{"RESOURCE_STALLS.FPCW", 0xA2, 0x20, "Stall cycles due to FPU control word write"},
     //{"RESOURCE_STALLS.MXCSR", 0xA2, 0x40, "Stall cycles due to MXCSR register rename occurring too close to another MXCSR rename"},
     //{"RESOURCE_STALLS.OTHER", 0xA2, 0x80, "Stall cycles due to other stalls, e.g. TAP, MOSBDrain, MS-uip"}
-    /*
+    {"L1D.REPL", 0x51, 0x01, "Number of lines brought into the L1D cache"},
+    {"MEM_LOAD_RETIRED.L1D_HIT", 0xCB, 0x01, "Number of retired loads that hit the L1 data cache"},
+    {"MEM_LOAD_RETIRED.L2_HIT", 0xCB, 0x02, "Number of retired loads that hit the L2 cache"},
+    //{"MEM_LOAD_RETIRED.L3_UNSHARED_HIT", 0xCB, 0x04, "Number of retired loads that hit their own, unshared line in the L3 cache"},
+    //{"MEM_LOAD_RETIRED.L3_MISS", 0xCB, 0x10, "Number of retired loads that missed the L3 cache"},
+    //{"MEM_LOAD_RETIRED.HIT_LFB", 0xCB, 0x40, "Number of retired loads that miss the L1D and the address is located in an allocated line fill buffer and will soon be committed to cache"},
+    //{"MEM_LOAD_RETIRED.DTLB_MISS", 0xCB, 0x80, "Number of retired loads that missed the DTLB"},
     {"LOAD_BLOCK.L1D", 0x03, 0x20, "Loads blocked by the L1 data cache, e.g. too many outstanding misses"},
-    {"DTLB_MISSES.ANY", 0x08, 0x01, "Memory accesses that missed the DTLB"},
-    {"LOAD_DISPATCH.ANY", 0x13, 0x04, "Loads dispatched from the reservation station"},
-    {"BR_MISP_EXEC.ANY", 0x89, 0x7F, "Number of mispredicted near branch insts that were executed, but not necessarily retired"}
-    */
+    //{"DTLB_MISSES.ANY", 0x08, 0x01, "Memory accesses that missed the DTLB"},
+    //{"LOAD_DISPATCH.ANY", 0x13, 0x04, "Loads dispatched from the reservation station"},
+    //{"BR_MISP_EXEC.ANY", 0x89, 0x7F, "Number of mispredicted near branch insts that were executed, but not necessarily retired"},
 };
-
+#endif
 
 int main(int argc, char** argv) {
     
     if(argc != 3) {
-      std::cout << "./a.out <total loops> <num streams>\n";
-      exit(1);
+        std::cout << "./a.out <total loops> <num streams>\n";
+        exit(1);
     }
     unsigned loops = atoi(argv[1]);
     unsigned streams = atoi(argv[2]);
     uint64_t nodeIdx[streams];
     uint64_t sums[streams];
     
-    uint32_t arraySize = 4*1024*1024; // 64M-entries
+    uint32_t arraySize = 1024; // 64M-entries
     std::cout << "Loops: " << loops << ", Streams: " << streams << "\n";
-    std::cout << "Trying to initialize array size = " << arraySize/1024 << "K entries = " << arraySize*sizeof(uint32_t)/1024 << "KB\n";
+    std::cout << "Trying to initialize array size = " << arraySize/1024 << "K entries = " << arraySize*sizeof(uint32_t)/1024 << "KB\n\n";
 
+#ifdef PERFMON
     initPCM(WSMEvents);
+#endif
     
     std::vector<uint32_t> arrayTmp;
     std::vector<uint32_t> array;
@@ -75,17 +88,28 @@ int main(int argc, char** argv) {
         lastArray[i] = i; //rand() % arraySize;
     }
     std::cout << "Starting...\n";
-    //zsim_roi_begin();
+#ifdef ZSIM
+    zsim_roi_begin();
+#endif
+#ifdef PERFMON
     SystemCounterState before_sstate = getSystemCounterState();
+#endif
+    
     for(uint64_t i = 0; i < (loops/streams)*arraySize; i++) {
         for(int j = 0; j < streams; j++) {
+            //std::cout << "lastArray: " << lastArray[j] << ", ";
 	    lastArray[j] = array[lastArray[j]];
+            //std::cout << "new lastArray: " << lastArray[j] << "\n";
 	    arraySums[j] += lastArray[j];
         }
     }
-    //zsim_roi_end();
+#ifdef ZSIM
+    zsim_roi_end();
+#endif
+#ifdef PERFMON
     SystemCounterState after_sstate = getSystemCounterState();
     printStats(WSMEvents, before_sstate, after_sstate);
+#endif
     
     std::cout << "Results: ";
     for(int i = 0; i < streams; i++) {
--- Sim/sssp_dijkstra.cpp
+++ Sim/sssp_dijkstra.cpp
@@ -6,31 +6,46 @@
 #include <time.h>
 #include <queue>
 #include <vector>
-
-#include <cpucounters.h>
+#include <papi.h>
 
 #include "Graph.h"
 #include "Worklist.h"
 
-struct PCMEvent {
-    const char * name;
-    unsigned char event;
-    unsigned char umask;
-    const char * description;
-};
+#define PERFMON
+
+#ifdef ZSIM
+#include "zsim_hooks.h"
+#endif
+
+#ifdef PERFMON
+#include "pcm.h"
 
 PCMEvent WSMEvents[4] = {
     {"RESOURCE_STALLS.ANY", 0xA2, 0x01, "Allocator resource related stalls, including PRF, LSQ, branch mispred, etc."},
     {"RESOURCE_STALLS.LOAD", 0xA2, 0x02, "Stall cycles due to lack of load buffer entries"},
     {"RESOURCE_STALLS.RS_FULL", 0xA2, 0x04, "Stall cycles due to lack of reservation station entries"},
-    {"RESOURCE_STALLS.ROB_FULL", 0xA2, 0x10, "Stall cycles due to lack of ROB entries"}
-    /*
-    {"LOAD_BLOCK.L1D", 0x03, 0x20, "Loads blocked by the L1 data cache, e.g. too many outstanding misses"},
-    {"DTLB_MISSES.ANY", 0x08, 0x01, "Memory accesses that missed the DTLB"},
-    {"LOAD_DISPATCH.ANY", 0x13, 0x04, "Loads dispatched from the reservation station"},
-    {"BR_MISP_EXEC.ANY", 0x89, 0x7F, "Number of mispredicted near branch insts that were executed, but not necessarily retired"}
-    */
+    {"RESOURCE_STALLS.STORE", 0xA2, 0x08, "Stall cycles due to lack of store buffer entries"},
+    //{"L1D.PEND_MISS.PENDING", 0x48, 0x01, "Increments the number of outstanding L1D misses every cycle. Set Cmask = 1 and Edge = 1 to count occurrences"}
+    //{"RESOURCE_STALLS.ROB_FULL", 0xA2, 0x10, "Stall cycles due to lack of ROB entries"},
+    //{"RESOURCE_STALLS.FPCW", 0xA2, 0x20, "Stall cycles due to FPU control word write"},
+    //{"RESOURCE_STALLS.MXCSR", 0xA2, 0x40, "Stall cycles due to MXCSR register rename occurring too close to another MXCSR rename"},
+    //{"RESOURCE_STALLS.OTHER", 0xA2, 0x80, "Stall cycles due to other stalls, e.g. TAP, MOSBDrain, MS-uip"}
+    //{"L1D.REPL", 0x51, 0x01, "Number of lines brought into the L1D cache"},
+    //{"MEM_LOAD_UOPS_RETIRED.L1D_HIT", 0xD1, 0x01, "Number of retired loads that hit the L1 data cache"},
+    //{"MEM_LOAD_UOPS_RETIRED.L2_HIT", 0xD1, 0x02, "Number of retired loads that hit the L2 cache"},
+    //{"MEM_LOAD_UOPS_RETIRED.LLC_MISS", 0xD1, 0x20, "Number of retired loads that missed the L3 cache"},
+    //{"MEM_LOAD_UOPS_RETIRED.LLC_HIT", 0xD1, 0x04, "Number of retired loads that hit their own, unshared line in the L3 cache"},
+    //{"MEM_LOAD_RETIRED.HIT_LFB", 0xCB, 0x40, "Number of retired loads that miss the L1D and the address is located in an allocated line fill buffer and will soon be committed to cache"},
+    //{"MEM_LOAD_RETIRED.DTLB_MISS", 0xCB, 0x80, "Number of retired loads that missed the DTLB"},
+    //{"LOAD_BLOCK.L1D", 0x03, 0x20, "Loads blocked by the L1 data cache, e.g. too many outstanding misses"},
+    //{"DTLB_MISSES.ANY", 0x08, 0x01, "Memory accesses that missed the DTLB"},
+    //{"LOAD_DISPATCH.ANY", 0x13, 0x04, "Loads dispatched from the reservation station"},
+    //{"BR_MISP_EXEC.ANY", 0x89, 0x7F, "Number of mispredicted near branch insts that were executed, but not necessarily retired"},
 };
+
+#endif
+
+
 
 int main(int argc, char** argv) {
     
@@ -47,18 +62,13 @@ int main(int argc, char** argv) {
     if(argc == 5) {
         genOutput = true;
     }
-    uint64_t iters = 0;
-    uint64_t workPerCurIter = 0;
-    uint64_t workGenPerCurIter = 0;
-    uint64_t conflictsPerCurIter = 0;
-    uint64_t totalWork = 0;
-    uint64_t totalWorkIssued = 0;
-    uint64_t totalWorkGen = 0;
-    uint64_t totalConflicts = 0;
-    bool infCores = false;
-    uint64_t maxCores = 128;
-    uint64_t maxWork = 0;
+
     
+#ifdef PERFMON
+    initPCM(WSMEvents);
+#endif
+
+
     Graph* graph = new Graph();
     std::priority_queue<Work, std::vector<Work>, ComparePriority> wl;
     
@@ -73,77 +83,18 @@ int main(int argc, char** argv) {
     Work initWork = {source, 0, 0};
     wl.push(initWork);
     graph->getNode(source)->payload = 0;
+
     
     t2 = time(NULL);
     
-    PCM * m = PCM::getInstance();
-    
-    PCM::ExtendedCustomCoreEventDescription conf;
-    conf.fixedCfg = NULL; // default
-    conf.nGPCounters = 4;
-    EventSelectRegister regs[4];
-    conf.gpCounterCfg = regs;
-    EventSelectRegister def_event_select_reg;
-    def_event_select_reg.value = 0;
-    def_event_select_reg.fields.usr = 1;
-    def_event_select_reg.fields.os = 1;
-    def_event_select_reg.fields.enable = 1;
-    for(int i=0;i<4;++i)
-        regs[i] = def_event_select_reg;
-    
-    for(int i = 0; i < 4; i++) {
-        regs[i].fields.event_select = WSMEvents[i].event;
-        regs[i].fields.umask = WSMEvents[i].umask;
-    }
-    
-    /*
-    // L1 hits
-    regs[0].fields.event_select = 0x80;
-    regs[0].fields.umask = 0x01;
-    // L1 misses
-    regs[1].fields.event_select = 0x80;
-    regs[1].fields.umask = 0x02;
-    // L1 reads
-    regs[2].fields.event_select = 0x80;
-    regs[2].fields.umask = 0x03;
-    // L1 cycle stalls
-    regs[3].fields.event_select = 0x80;
-    regs[3].fields.umask = 0x04;
-    */
-    
-    PCM::ErrorCode status = m->program(PCM::EXT_CUSTOM_CORE_EVENTS, &conf);
-    switch(status) {
-    case PCM::Success:
-        break;
-    case PCM::MSRAccessDenied:
-        std::cerr << "Access to Intel(r) Performance Counter Monitor has denied (no MSR or PCI CFG space access).\n";
-        exit(EXIT_FAILURE);
-    case PCM::PMUBusy:
-        std::cerr << "Access to Intel(r) Performance Counter Monitor has denied (Performance Monitoring Unit is occupied by other application). Try to stop the application that uses PMU.\n";
-        std::cerr << "Alternatively you can try to reset PMU configuration at your own risk. Try to reset? (y/n)\n";
-        char yn;
-        std::cin >> yn;
-        if ('y' == yn) {
-            m->resetPMU();
-            std::cerr << "PMU configuration has been reset. Try to rerun the program again.\n";
-        }
-        exit(EXIT_FAILURE);
-    default:
-        std::cerr << "Access to Intel(r) Performance Counter Monitor has denied (Unknown error).\n";
-        exit(EXIT_FAILURE);
-    }
-    
-    std::cerr << "\nDetected "<< m->getCPUBrandString() << " \"Intel(r) microarchitecture codename "<<m->getUArchCodename()<<"\"\n";
-    
-    
+        
+#ifdef PERFMON
     SystemCounterState before_sstate = getSystemCounterState();
+#endif
 
     Work work;
     
-    uint64_t idealIters = 0;
-    uint64_t idealWork = 0;
     while(!wl.empty()) {
-        //idealIters++;
         work = wl.top();
         wl.pop();
         Node* curNode = graph->getNode(work.graphId);
@@ -152,43 +103,23 @@ int main(int argc, char** argv) {
             Node* destNode = graph->getNode(edge->dest);
             if(curNode->payload + edge->weight < destNode->payload) {
                 destNode->payload = curNode->payload + edge->weight;
-                Work newWork = {edge->dest, destNode->payload/50000, iters+1};
+                Work newWork = {edge->dest, destNode->payload/50000, 0};
                 wl.push(newWork);
-                //idealWork++;
             }
         }
     }
-    std::cout << "IdealIters: " << idealIters << ", IdealWork: " << idealWork << "\n";
     
-    
+#ifdef PERFMON
     SystemCounterState after_sstate = getSystemCounterState();
-    
-    uint64_t insts = getInstructionsRetired(before_sstate, after_sstate);
+    printStats(WSMEvents, before_sstate, after_sstate);
+#endif
 
-    for(int i = 0; i < 4; i++) {
-        uint64_t counter = getNumberOfCustomEvents(i, before_sstate, after_sstate);
-        std::cout << WSMEvents[i].name << ": " << counter << ", PKI: " << double(counter)*1000.0/double(insts) << "\n";
-    }
     
-    std::cout << "Instructions retired: " << insts << "\n";
-    std::cout << "IPC: " << getIPC(before_sstate, after_sstate) << "\n";
-    /*
-    std::cout << "Instructions per clock: " << getIPC(before_sstate,after_sstate)
-              << "\nL3 cache hit ratio: " << getL3CacheHitRatio(before_sstate,after_sstate)
-              << "\nBytes read: " << getBytesReadFromMC(before_sstate,after_sstate)
-              << "\n";
-    */
     t3 = time(NULL);
-    m->cleanup();
     
     std::cout << "Setup time: " << (t2 - t1) << " seconds\n";
     std::cout << "SSSP time: " << (t3 - t2) << " seconds\n";
     
-    
-    std::cout << "Iters: " << iters << ", totalWork: " << totalWork << ", max cores active: " << maxWork 
-              << ", utilization: " << double(totalWorkIssued)/double(iters*maxCores) 
-              << ", executed: " << double(totalWork)/double(iters*maxCores) 
-              << ", conflict percent: " << double(totalConflicts)/double(iters*maxCores) << std::endl;
     
     if(genOutput) {
         for(int i = 0; i < graph->numNodes; i++) {
@@ -196,5 +127,4 @@ int main(int argc, char** argv) {
         }
         out.close();
     }
-    
 }
